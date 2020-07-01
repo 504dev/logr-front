60364aac6cd3d012e2a49887125f63c7051cae92
Mona Lisa <nikruzhok@gmail.com>
2020-07-01 18:42:45 +0300

    conf

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,7 +3,5 @@ const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"',
-  VUE_APP_REST: '"http://api.logr.loc:7778"',
-  VUE_APP_WS: '"ws://api.logr.loc:7778/ws"'
+  NODE_ENV: '"development"'
 })
--- config/prod.env.js
+++ config/prod.env.js
@@ -1,4 +1,6 @@
 'use strict'
 module.exports = {
-  NODE_ENV: '"production"'
+  NODE_ENV: '"production"',
+  VUE_APP_REST: `"${process.env.VUE_APP_REST || ''}"`,
+  VUE_APP_WS: `"${process.env.VUE_APP_WS || ''}"`
 }
