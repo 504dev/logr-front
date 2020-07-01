01de912dcdd4de19922abc09420868dbc9d2568a
Mona Lisa <nikruzhok@gmail.com>
2020-07-01 18:49:34 +0300

    conf

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,5 +3,7 @@ const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"'
+  NODE_ENV: '"development"',
+  VUE_APP_REST: '"http://localhost:7778"',
+  VUE_APP_WS: '"ws://localhost:7778/ws"'
 })
