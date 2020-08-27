68462a8e1a3c9f8aa6aa113e029dfeb95a5d72f5
Mona Lisa <nikruzhok@gmail.com>
2020-08-27 16:23:06 +0300

    refact

--- src/pages/CountsPage.vue
+++ src/pages/CountsPage.vue
@@ -154,7 +154,7 @@ export default {
                 .map(({ data }, hostname) => {
                   data = this.filled(data.map(([x, y]) => [x * 1000, y]).reverse())
                   const hex = this.convertToHex(hostname)
-                  const color = '#' + hex.slice(0, 4) + hex.slice(-2)
+                  const color = '#' + hex.slice(0, 6)
                   return { name: hostname, data, color }
                 })
                 .sortBy('name')
